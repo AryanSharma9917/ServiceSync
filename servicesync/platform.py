82679864b124cbd869eac835adfb27d7157887c9
aryansharma9917 <sharmaaryan9837@gmail.com>
2024-06-01 22:18:59 +0530

    Added PLATFORM.py file

--- servicesync/platform.py
+++ servicesync/platform.py
@@ -5,3 +5,10 @@ from pygit2 import clone_repository, Keypair, RemoteCallbacks
 from . import util
 from . import errors
 
+class Platform:
+    """Platform holds the general metadata and functionality about the 
+    platform config.
+    """
+    name = None
+    version = None
+    _components = []
