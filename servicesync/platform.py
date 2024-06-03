39d0c8cb91194f408f3619e698b057c766bc8bf5
aryansharma9917 <sharmaaryan9837@gmail.com>
2024-06-03 10:39:41 +0530

    Added orogin file to the project

--- servicesync/platform.py
+++ servicesync/platform.py
@@ -5,6 +5,7 @@ from pygit2 import clone_repository, Keypair, RemoteCallbacks
 from . import util
 from . import errors
 
+
 class Platform:
     """Platform holds the general metadata and functionality about the 
     platform config.
@@ -39,6 +40,7 @@ class Platform:
                                 url=component.get('url'),
                             )
                         )
+
     def __repr__(self):
         return self.version
 
@@ -77,8 +79,78 @@ class Platform:
             component.location = match.get('url')
             if not component.url:
                 raise errors.NoComponentLocationDefined()
-            
+
     def fetch_components(self, repo_path, ssh_pub_key, ssh_priv_key):
         workspace = path.join(repo_path, self.name)
         for component in self._components:
             component.fetch(workspace, ssh_pub_key, ssh_priv_key)
+
+
+class MicroService:
+    """MicroService holds the microservice config and relevant
+    functionality.
+    """
+    refs = None
+    hash = None
+    url = None
+    
+    def __init__(self, alias, url):
+        self.alias = alias
+        self.url = url
+
+    def __repr__(self):
+        return self.alias
+
+    def __eq__(self, alias):
+        return self.alias == alias
+
+    def fetch(self, workspace, ssh_pub_key, ssh_priv_key, validate=False):
+        """This function will try to create the workspace for the component
+        that will be cloned. It will fetch the component and will export the
+        environment variable that points to component cloned repository.
+        """
+        self._clone(workspace, ssh_pub_key, ssh_priv_key)
+        if validate:
+            self._export_env(workspace)
+
+    def _clone(
+        self,
+        workspace,
+        ssh_pub_key,
+        ssh_priv_key,
+        ssh_user='git',
+        ssh_pass='',
+    ):
+        ws = util.create_workspace(workspace, self.alias)
+        keypair = Keypair(
+            username=ssh_user,
+            pubkey=ssh_pub_key,
+            privkey=ssh_priv_key,
+            passphrase=ssh_pass,
+        )
+
+        try:
+            cb = RemoteCallbacks(credentials=keypair)
+            repo = clone_repository(
+                url=self.url,
+                path=ws,
+                callbacks=cb,
+            )
+            try:
+                repo.checkout(self.refs)
+                print(f'Cloned {repo} to {ws}')
+            except Exception as err:
+                raise errors.CannotFetchRef(
+                    f'Cannot fetch ref: {self.refs}'
+                )
+        except Exception as err:
+                raise errors.CannotCloneRepository(
+                    f'Cannot clone repository: {err}'
+                )
+
+    def _export_env(self, workspace):
+        """Used to export the path of the cloned git repository.
+        Useful in CI/CD platform pipelines.
+        """
+        _name = f'{self.alias}_WORKSPACE'.upper()
+        environ[_name] = workspace
