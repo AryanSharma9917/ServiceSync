9ecfe606ad4260fa6dfae74657cb68bee1c2d666
aryansharma9917 <sharmaaryan9837@gmail.com>
2024-06-03 21:47:02 +0530

    Added config_files

--- servicesync/util.py
+++ servicesync/util.py
@@ -8,3 +8,15 @@ from .platform import Platform, MicroService
 from . import errors
 
 
+def open_file(config_file):
+    """Opens a file and sends its contents to be consumer by the yaml library.
+    :param config_file: string full path to the config file of the platform.
+    :return: a generator object that contains the YAML spec of the file.
+    """
+    try:
+        with open(config_file) as config:
+            return yaml.safe_load_all(config.read())
+    except FileNotFoundError:
+        raise errors.NoConfigFileFound(
+            f"""No 'config.yaml' file found in directory!"""
+        )
