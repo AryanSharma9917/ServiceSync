56ce73d923acef48170cc04b6705e33cd0165bd3
aryansharma9917 <sharmaaryan9837@gmail.com>
2024-06-04 23:09:13 +0530

    Added workspace path and version to the util file

--- servicesync/util.py
+++ servicesync/util.py
@@ -20,3 +20,43 @@ def open_file(config_file):
         raise errors.NoConfigFileFound(
             f"""No 'config.yaml' file found in directory!"""
         )
+    
+def get_versions_paths(workspace_path, version):
+    """Walks the specified directory where the platform versioning is located
+    and will try to find all the directories that match a regex of v[0-9]+.
+    e.g.: v1, v2, v3.
+    :param workspace_path: string path to the workspace where the 
+    configuration is located.
+    :param version: string the specific version to search for.
+    :return: a list of all yaml files present in the specified version
+    directory.
+    """
+    reg_vers = re.compile(version.split('.')[0])
+    vers_dir = next((path for path in listdir(workspace_path) if reg_vers.match(path)), None)
+
+    if not vers_dir:
+        raise errors.NoVersionFoundError(
+            f"""No version directory for version: {version} was found in 
+            {workspace_path}"""
+        )
+
+    vers_path = path.join(workspace_path, vers_dir)
+    reg_vers_match = version.split('.')
+
+    try:
+        reg_vers_file = re.compile(f'{reg_vers_match[0]}.{reg_vers_match[1]}')
+    except IndexError:
+        raise errors.SemverNonCompliantError(
+        f"""Semver non-compliant: The requested version ({version}) needs at
+        least a minor version"""
+      )
+
+    versions = list(filter(lambda p: re.search(reg_vers_file, p), listdir(vers_path)))
+
+    if not versions:
+        raise errors.NoVersionFoundError(
+            f"""No version {version} has been defined in path {vers_path}"""
+        )
+
+    return list(map(lambda p: path.join(vers_path, p), versions))
+
