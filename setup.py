d215f7c0c510ccb89e58a4c9121e01b946d8eea8
aryansharma9917 <sharmaaryan9837@gmail.com>
2024-06-07 13:28:41 +0530

    UPdated setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+VERSION='0.1.0'
+
+long_description = """
+# ServiceSync is a CI/CD for pinning microservices under a unifired version.
+"""
+
+packages=[
+    'click',
+    'pyyaml',
+    'tabulate',
+    'pygit2'
+]
+
