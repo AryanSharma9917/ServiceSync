f89d068aba97ef75494615f35c062d6de794c34d
aryansharma9917 <sharmaaryan9837@gmail.com>
2024-06-14 22:46:34 +0530

    Modified setups file

--- setup.py
+++ setup.py
@@ -2,14 +2,36 @@ from setuptools import setup, find_packages
 
 VERSION='0.1.0'
 
-long_description = """
-# ServiceSync is a CI/CD for pinning microservices under a unifired version.
+long_description="""
+Hurd is a CI/CD tool for pinning microservices under a unified platform.
 """
 
 packages=[
-    'click',
-    'pyyaml',
-    'tabulate',
-    'pygit2'
+    'hurd',
 ]
 
+install_requires=[
+    'click',
+    'pygit2',
+    'pyyaml',
+    'tabulate',
+]
+
+def main():
+    setup_info = dict(
+        name='hurd',
+        version=VERSION,
+        url="https://github.com/pi-victor/hurd",
+        author='Victor Palade <victor@cloudflavor.io>',
+        description='A microservice platform tool for CI/CD pipelines',
+        long_description=long_description,
+        license='Apache-2.0',
+        packages=packages,
+        install_requires=install_requires,
+        zip_safe=False,
+    )
+
+    setup(**setup_info)
+
+if __name__ == '__main__':
+    main()
